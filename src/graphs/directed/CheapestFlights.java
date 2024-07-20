495932dfb7bb0b85b2c9c2abdb10d6fe5e4c067d
ankur <ankurbansal153@gmail.com>
2024-07-20 13:59:24 +0530

    CheapestFlights Within K Stops Algorithm in Graph

---
+++ src/graphs/directed/CheapestFlights.java
@@ -0,0 +1,76 @@
+package graphs.directed;
+
+import graphs.Edge;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//Cheapest Flights Within K Stops
+public class CheapestFlights {
+    static class Info{
+        int v;
+        int cost;
+        int stops;
+
+        public Info(int v, int c, int s){
+            this.v = v;
+            this.cost = c;
+            this.stops = s;
+        }
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] flight = {{0,1,100},{1,2,100},{2,0,100},{1,3,600},{2,3,200}};
+        int src = 0, dst = 3, k = 1;
+        System.out.println(cheapestFlight(n,flight,src,dst,k));
+
+
+    }
+    public static int cheapestFlight(int n, int[][] flights, int src, int dest, int k) {
+        ArrayList<Edge>[] graph = new ArrayList[n];
+        createGraph(flights, graph);
+
+        int[] dist = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        Queue<Info> q = new LinkedList<>();
+        q.add(new Info(src, 0, 0));
+
+        while (!q.isEmpty()) {
+            Info curr = q.remove();
+            if (curr.stops > k) {
+                continue;
+            }
+            for (int i = 0; i < graph[curr.v].size(); i++) {
+                Edge e = graph[curr.v].get(i);
+                int v = e.dest;
+                int wt = e.wt;
+
+                if (curr.cost + wt < dist[v] && curr.stops <= k) {
+                    dist[v] = curr.cost + wt;
+                    q.add(new Info(v, dist[v], curr.stops + 1));
+                }
+            }
+        }
+
+        return dist[dest] == Integer.MAX_VALUE ? -1 : dist[dest];
+    }
+    public static void createGraph(int[][] flights, ArrayList<Edge>[] graph){
+        for(int i = 0; i<graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] flight : flights) {
+            int src = flight[0];
+            int dest = flight[1];
+            int wt = flight[2];
+
+            Edge e = new Edge(src, dest, wt);
+            graph[src].add(e);
+        }
+    }
+
+}
