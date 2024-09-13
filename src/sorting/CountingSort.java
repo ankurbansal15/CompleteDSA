77670a3f834e015793deb3903bd516c9a6525e3b
ankurbansal15 <ankurbansal153@gmail.com>
2024-09-13 22:19:35 +0530

    Basic Sorting Algorithm's

--- src/sorting/CountingSort.java
+++ src/sorting/CountingSort.java
@@ -18,32 +18,23 @@ public class CountingSort {
 
         int largest = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
-
             largest = Math.max(largest, arr[i]);
-
         }
 
-        //making count/frequency array
         int[] count = new int[largest + 1];
 
         for (int i = 0; i < arr.length; i++) {
-
             count[arr[i]]++;
         }
 
-        //sorting
         int j = 0;
         for (int i = 0; i < count.length; i++) {
 
             while (count[i] > 0) {
-                //adding element to the original array
                 arr[j] = i;
-                //if repeated then adding again until the count became zero
                 count[i]--;
-                //increasing the index so to element in next box of array
                 j++;
             }
-
         }
     }
 }
--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -5,28 +5,20 @@ public class InsertionSort {
     public static void main(String[] args){
 
         int[] array = {1,2,3,5,4};
+        insertionSort(array);
         for (int j = 0; j < array.length; j++) {
-
             System.out.print(array[j]+" ");
         }
-        System.out.print("------");
-
-        insertionSort(array);
 
     }
 
     public static void insertionSort(int[] array){
 
         int n = array.length;
-
-        // run loop n-1 times because last element will be automaticaly sorted
-        //also start the loop from 1 because we have taken loop works on the condition when it is greater 0
         
         for(int i = 1; i<n; i++){
             int current = array[i];
             int previous = i-1;
-
-            //finding out correct position to insert
 
             while( previous >= 0 && array[previous] > current){
                 
@@ -37,11 +29,6 @@ public class InsertionSort {
             //insertion
             array[previous+1] = current;
 
-            for (int j = 0; j < array.length; j++) {
-
-                System.out.print(array[j]+" "); 
-            }
-            System.out.print("------");
         }
     }
 }
