4aa7312f8649d953cdb3a191e70afba74477dae1
ankurbansal15 <ankurbansal153@gmail.com>
2024-09-27 00:16:07 +0530

    Divide & Conquer

---
+++ src/sorting/SearchInRotatedArray.java
@@ -0,0 +1,42 @@
+package sorting;
+
+public class SearchInRotatedArray {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println(search(arr, target, 0, arr.length));
+
+    }
+
+    public static int search(int[] arr, int target, int si, int ei) {
+
+        if (si > ei) {
+            return -1;
+        }
+
+        int mid = si + (ei - si) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (arr[si] <= arr[mid]) {
+            if (arr[si] <= target && target <= arr[mid]) {
+                return search(arr, target, si, mid - 1);
+            } else {
+                return search(arr, target, mid + 1, ei);
+            }
+
+        } else {
+            if (target >= arr[mid] && target <= arr[ei]) {
+                return search(arr, target, mid + 1, ei);
+            } else {
+                return search(arr, target, si, mid - 1);
+            }
+
+        }
+
+    }
+
+}
