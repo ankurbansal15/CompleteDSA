418bbe21f2a02c00db74239c75a4064f46506c85
ankurbansal15 <ankurbansal153@gmail.com>
2024-11-21 22:30:37 +0530

    Stack Complete

--- src/stacks/StockSpanProblem.java
+++ src/stacks/StockSpanProblem.java
@@ -4,39 +4,39 @@ import java.util.Stack;
 
 public class StockSpanProblem {
 
-    public static void main(String[] args){
-        int[] stock = {100,80,60,70,60,85,100};
+    public static void main(String[] args) {
+        int[] stock = {100, 80, 60, 70, 60, 85, 100};
         int[] span = new int[stock.length];
 
-        stockSpan(stock,span);
+        stockSpan(stock, span);
 
-        for(int i =0; i<span.length; i++){
+        for (int i = 0; i < span.length; i++) {
             System.out.println(span[i]);
         }
     }
 
-    public static void stockSpan(int []stock, int []span){
-        
-        Stack <Integer> s = new Stack<>();
+    public static void stockSpan(int[] stock, int[] span) {
+
+        Stack<Integer> s = new Stack<>();
         span[0] = 1;
         s.push(0);
 
-        for(int i = 1; i< stock.length; i++){
+        for (int i = 1; i < stock.length; i++) {
             int currPrice = stock[i];
 
-            while(!s.isEmpty() && currPrice >= stock[s.peek()]){
+            while (!s.isEmpty() && currPrice >= stock[s.peek()]) {
                 s.pop();
-            } 
+            }
 
-            if(s.isEmpty()){
-                span[i] = i+1;
-            }else{
+            if (s.isEmpty()) {
+                span[i] = i + 1;
+            } else {
                 int prevHigh = s.peek();
-                span[i] = i-prevHigh;
+                span[i] = i - prevHigh;
             }
             s.push(i);
         }
 
     }
-    
+
 }
