f186cdeee0295d38867d8c97836b8a3d0e39fb28
ankurbansal15 <ankurbansal153@gmail.com>
2024-12-26 12:46:12 +0530

    Bug fix

--- src/hashing/LargestSubArraySum0.java
+++ src/hashing/LargestSubArraySum0.java
@@ -13,6 +13,11 @@ public class LargestSubArraySum0 {
 
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
+
+            if (sum == 0) {
+                len = i + 1;
+            }
+
             if (map.containsKey(sum)) {
                 len = Math.max(len, i - map.get(sum));
 
