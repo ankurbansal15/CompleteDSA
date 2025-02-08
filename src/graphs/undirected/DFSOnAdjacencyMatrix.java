59ac5fba105dadd643c38069ceabb1e29ae0ac94
ankurbansal15 <ankurbansal153@gmail.com>
2025-02-08 19:19:10 +0530

    DFS on Adj Matrix

---
+++ src/graphs/undirected/DFSOnAdjacencyMatrix.java
@@ -0,0 +1,35 @@
+package graphs.undirected;
+
+public class DFSOnAdjacencyMatrix {
+
+    public static void main(String[] args) {
+        int[][] isConnected = {
+                {1, 1, 0},
+                {1, 1, 0},
+                {0, 0, 1}
+        };
+        System.out.println(findCircleNum(isConnected));
+
+    }
+    public static int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                count++;
+                dfs(isConnected, visited, i);
+            }
+        }
+        return count;
+    }
+
+    public static void dfs(int[][] isConnected, boolean[] visited, int i) {
+        visited[i] = true;
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                dfs(isConnected, visited, j);
+            }
+        }
+    }
+}
