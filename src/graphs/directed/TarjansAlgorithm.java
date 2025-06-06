2380935daedbe0e0a04367af531092abae4bd1b3
ankurbansal15 <ankurbansal153@gmail.com>
2025-06-06 11:51:39 +0530

    Bug fix

--- src/graphs/directed/TarjansAlgorithm.java
+++ src/graphs/directed/TarjansAlgorithm.java
@@ -21,36 +21,34 @@ public class TarjansAlgorithm {
         }
     }
     public static List<List<Integer>> tarjansAlgorithm(int n, List<List<Integer>> graph) {
+        int time = 0;
         int[] disc = new int[n];
         int[] low = new int[n];
-        boolean[] visited = new boolean[n];
         boolean[] inStack = new boolean[n];
         Stack<Integer> stack = new Stack<>();
         List<List<Integer>> sccList = new ArrayList<>();
-        int[] time = new int[]{0};
+
+        Arrays.fill(disc, -1);
+        Arrays.fill(low, -1);
 
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(i, graph, visited, disc, low, stack, inStack, time, sccList);
+            if (disc[i] == -1) {
+                dfs(i, graph, disc, low, inStack, stack, sccList, new int[]{time});
             }
         }
 
         return sccList;
     }
 
-    private static void dfs(int u, List<List<Integer>> graph,
-                     boolean[] visited, int[] disc, int[] low,
-                     Stack<Integer> stack, boolean[] inStack,
-                     int[] time, List<List<Integer>> sccList) {
-
-        visited[u] = true;
-        disc[u] = low[u] = time[0]++;
+    private static void dfs(int u, List<List<Integer>> graph, int[] disc, int[] low,
+                     boolean[] inStack, Stack<Integer> stack, List<List<Integer>> sccList, int[] timeRef) {
+        disc[u] = low[u] = timeRef[0]++;
         stack.push(u);
         inStack[u] = true;
 
         for (int v : graph.get(u)) {
-            if (!visited[v]) {
-                dfs(v, graph, visited, disc, low, stack, inStack, time, sccList);
+            if (disc[v] == -1) {
+                dfs(v, graph, disc, low, inStack, stack, sccList, timeRef);
                 low[u] = Math.min(low[u], low[v]);
             } else if (inStack[v]) {
                 low[u] = Math.min(low[u], disc[v]);
